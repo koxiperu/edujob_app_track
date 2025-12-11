5c1504335d66808e9f4ed304dfc88b7713fd7267
koxiperu <koxiperu@gmail.com>
2025-12-11 14:15:25 +0100

    added dependencies

---
+++ src/main/java/cnfpc/project/edujob_app_track/EdujobAppTrackApplication.java
@@ -0,0 +1,13 @@
+package cnfpc.project.edujob_app_track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdujobAppTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdujobAppTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cnfpc/project/edujob_app_track/EdujobAppTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package cnfpc.project.edujob_app_track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdujobAppTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
