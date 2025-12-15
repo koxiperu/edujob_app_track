55b7d349fcc9b235f9a6a3ec687796da746b4477
koxiperu <koxiperu@gmail.com>
2025-12-15 11:40:45 +0100

    added user profile view and edit

--- src/main/java/cnfpc/project/edujob_app_track/controller/DashboardController.java
+++ src/main/java/cnfpc/project/edujob_app_track/controller/DashboardController.java
@@ -1,19 +1,53 @@
 package cnfpc.project.edujob_app_track.controller;
 
 import java.security.Principal;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import cnfpc.project.edujob_app_track.model.Application;
+import cnfpc.project.edujob_app_track.model.Enums.ApplicationStatus;
+import cnfpc.project.edujob_app_track.model.User;
+import cnfpc.project.edujob_app_track.repository.ApplicationRepository;
+import cnfpc.project.edujob_app_track.repository.UserRepository;
 
 @Controller
 public class DashboardController {
 
+    private final UserRepository userRepository;
+    private final ApplicationRepository applicationRepository;
+
+    public DashboardController(UserRepository userRepository,
+                               ApplicationRepository applicationRepository) {
+        this.userRepository = userRepository;
+        this.applicationRepository = applicationRepository;
+    }
+    
     @GetMapping("/dashboard")
-    public String dashboard(Model model, Principal principal) {
+    public String dashboard(
+            Model model,
+            Principal principal,
+            @RequestParam(value = "status", required = false) ApplicationStatus status
+    ) {
+        User user = userRepository.findByUsername(principal.getName()).orElseThrow();
+
+        List<Application> applications;
+
+        if (status != null) {
+            applications = applicationRepository.findByUserAndStatus(user, status);
+        } else {
+            applications = applicationRepository.findByUser(user);
+        }
+
+        model.addAttribute("applications", applications);
         model.addAttribute("username", principal.getName());
         model.addAttribute("title", "Dashboard");
-
-        return "layout/base"; // render base.html
+        model.addAttribute("statuses", ApplicationStatus.values());
+        model.addAttribute("selectedStatus", status);
+        return "dashboard";
     }
 }
 
